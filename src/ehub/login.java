e8388a2be386697c568690d6c15a7f3865b3c710
J <73541905+janvi213@users.noreply.github.com>
2021-12-17 13:30:16 +0530

    Update login.java

--- src/ehub/login.java
+++ src/ehub/login.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @SuppressWarnings("serial")
 
